5ac7f18300ce600b9a710c0b663fce05453366a8
OliverHorrobin <74053426+OliverHorrobin@users.noreply.github.com>
2021-03-24 16:49:23 +0000

    Update Gauss_runner.m

--- Gauss_runner.m
+++ Gauss_runner.m
@@ -1,5 +1,9 @@
+clear all
+close all
+%(a,b,x,y,xBar,yBar,amplitude,Rotation_angle,noise)
+%When b is more than a, the length of the beam in y is more than in x, and the major axis angle reflects this accordingly
 
-genGauss(60,9,150,150,75,60,250)
+genGauss(30,60,200,200,100,100,250,1,0);
 Centroid(Beam);
 d4sigma(Beam);
 oneOverESquared(Beam);
