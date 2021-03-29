7920f3605085529acc161b76949482352a11afb9
OliverHorrobin <74053426+OliverHorrobin@users.noreply.github.com>
2021-03-29 17:43:57 +0100

    Update WSAO.m
    Made it run to optimise circle first ie original program then made it run a second time to optimise to specified parameter. Made a new error function to do this so it minimises the difference between what you have and what you want

--- WSAO.m
+++ WSAO.m
@@ -1,10 +1,11 @@
 % Use CTRL+C in command window to stop simulation
 
-resolution  = 100;  % Resolution of all surfaces
+
+resolution  = 200;  % Resolution of all surfaces
 orders      = 36;   % Aberration orders
 channels    = 36;   % Number of mirror channels
 crop_amount = 10;   % Amount to remove from polys
-iterations  = 42;   % Optimisation Iterations
+iterations  = 20;   % Optimisation Iterations
 dm_response = 0.1;  % Mirror response time in sec
 
 %rng_seed = 1122;
@@ -33,34 +34,42 @@ FF.settings(4,0.2,0.07,0.07);
 
 % Generate initial wavefront & farfield
 coeffs = 0.5*randn(orders,1);
-initwf = coeff_surface(coeffs,polys);
-initff = FF.generate_farfield(1,initwf)*1e-6;
+initwf = coeff_surface(coeffs,polys);%Original
 
+initff = FF.generate_farfield(1,initwf)*1e-6; %ORIGINAl
+% initff = genGauss(30,30,200,200,100,100,250,140,0);% NOT ORIGINAL
 
 % Create solver object to solve objective function
 fhandle = @(r) cost_function(r,initwf,mirror,FF);
-solver = pattern_search(channels,fhandle); % Chose one from optimisation algorithms folder
+fhandle2 = @(r) cost_function2(r,initwf,mirror,FF);
+
+% solver = coordinate_search(channels,fhandle); % Chose one from optimisation algorithms folder <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+solver = pattern_search(channels,fhandle);
+solver2 = pattern_search(channels,fhandle2);%not original
+% solver = adam_optimiser(channels,fhandle);
 %solver.settings(0.01,0.1,0.1,0);
 
 fig = figure(1);
 axis tight manual
 fig.Position = [35 0 706 871];
-tiledlayout(4,2,'TileSpacing','compact')
+tiledlayout(4,2,'TileSpacing','compact')%Not original
 
 nexttile(1,[1 1])
-plotWF(initwf);
+plotWF(initwf); %<<<<<<<<<<<<<<<<<
 title("Input wavefront")
 
 nexttile(2,[1,1])
-plotFF(initff);
+plotFF(initff); %<<<<<<<<<<<<<<<<<
 title("Initial farfield")
 
 % Prealocating cost and evaluation arrays
-cost = nan(iterations,1);
+cost = nan(iterations,1) %ORIGINAL
+% cost = 22 %Not original
 evals = nan(iterations,1);
 ds1 = arrayDatastore(initwf)
 data = read(ds1)
 % Closed Loop
+
 for i = 1:iterations
     
     solver.step();
@@ -69,18 +78,18 @@ for i = 1:iterations
     v = solver.position*100;
     mirror.set_channels(v);
     outwf = initwf + mirror.shape;
-    outff = FF.generate_farfield(1,outwf)*1e-6;
+    outff = FF.generate_farfield(1,outwf)*1e-6; %ORIGINAL
     
     % Log current cost and number of evals
     cost(i) = fhandle(solver.position);
     evals(i) = solver.evaluations;
     
     nexttile(3,[1,1])
-    plotWF(outwf);
+    plotWF(outwf); %<<<<<<<<<<<<<<<<<
     title("Output wavefront")
     
     nexttile(4,[1,1])
-    plotFF(outff);
+    plotFF(outff); %<<<<<<<<<<<<<<<<
     title("Current farfield")
     %subplot(3,2,5:6)
     
@@ -92,19 +101,84 @@ for i = 1:iterations
     txt = sprintf("est.Duration: %.2f min",solver.evaluations*dm_response/60);
     text(0.6,0.85,txt,'Units','normalized','FontSize',14)
 
-    [xCentroid,yCentroid]= Centroid(outff);
+
+    [xCentroid,yCentroid]= Centroid(outff);%NOT original
     txt = sprintf("xCentroid: %.2f, yCentroid: %.2f",xCentroid,yCentroid);
     text(0.0,-0.6,txt,'Units','normalized','FontSize',10)
     
-    [ooesPercent,ooesCircDia]= oneOverESquared(outff);
+
+    [ooesPercent,ooesCircDia]= oneOverESquared(outff);%Not original
     txt = sprintf("ooesPercent: %.2f, ooesCircDia: %.2f",ooesPercent,ooesCircDia);
     text(0.6,-0.6,txt,'Units','normalized','FontSize',10)
     
-    [xD4S,yD4S,Major_axis_angle] = d4sigma(outff);
+
+    [xD4S,yD4S,Major_axis_angle] = d4sigma(outff);%Not original
     txt = sprintf("xD4S: %.2f, yD4S: %.2f, Major axis angle:%.2f ",xD4S,yD4S,Major_axis_angle);
     text(0.0,-1,txt,'Units','normalized','FontSize',10)
     
     drawnow
+end
+
+for i = iterations:(iterations*2) %not original
+    
+    solver2.step();
+    
+    % Get voltages and plot wavefront & farfield
+    v = solver2.position*100;
+    mirror.set_channels(v);
+    outwf = initwf + mirror.shape;
+    outff = FF.generate_farfield(1,outwf)*1e-6; %ORIGINAL
+    
+    % Log current cost and number of evals
+    cost(i) = fhandle(solver2.position);
+    evals(i) = solver2.evaluations;
+    
+    nexttile(3,[1,1])
+    plotWF(outwf); %<<<<<<<<<<<<<<<<<
+    title("Output wavefront")
+    
+    nexttile(4,[1,1])
+    plotFF(outff); %<<<<<<<<<<<<<<<<
+    title("Current farfield")
+    %subplot(3,2,5:6)
+    
+    nexttile(5,[1,2]);
+    plot(cost);
+    title("Performance");
+    xlabel("Evaluations");
+    ylabel("Cost");
+    txt = sprintf("est.Duration: %.2f min",solver2.evaluations*dm_response/60);
+    text(0.6,0.85,txt,'Units','normalized','FontSize',14)
+
+
+    [xCentroid,yCentroid]= Centroid(outff);%NOT original
+    txt = sprintf("xCentroid: %.2f, yCentroid: %.2f",xCentroid,yCentroid);
+    text(0.0,-0.6,txt,'Units','normalized','FontSize',10)
+    
+
+    [ooesPercent,ooesCircDia]= oneOverESquared(outff);%Not original
+    txt = sprintf("ooesPercent: %.2f, ooesCircDia: %.2f",ooesPercent,ooesCircDia);
+    text(0.6,-0.6,txt,'Units','normalized','FontSize',10)
+    
+
+    [xD4S,yD4S,Major_axis_angle] = d4sigma(outff);%Not original
+    txt = sprintf("xD4S: %.2f, yD4S: %.2f, Major axis angle:%.2f ",xD4S,yD4S,Major_axis_angle);
+    text(0.0,-1,txt,'Units','normalized','FontSize',10)
+    
+    drawnow
+end
+
+
+function error = Errorfunct(FarF,want) %Function to calculate the cost which is the fifference between what you want and what you have
+[xD4S,yD4S,Major_axis_angle] = d4sigma(FarF);
+[ooesPercent,ooesCircDia]= oneOverESquared(FarF);
+have = Major_axis_angle; %Set the parameter to meet here
+error = want - have;
+    if error > 0
+        error = error;
+    else
+        error = -error;%Used to keep the difference positive as the program tries to minamalise cost(error)
+    end
 end
 
 function cost = cost_function(pos,wf,mirror,FF)
@@ -115,25 +189,25 @@ function cost = cost_function(pos,wf,mirror,FF)
     ff = FF.generate_farfield(1,outwf)*1e-6;
     ff = medfilt2(ff,[3 3]); % Filter to reduce noise
     maxI = max(ff,[],'all');
-    % cost = -maxI; % Original
+    cost = -maxI; %ORIGINAL
+    % cost = sqrt(sum(outwf(:).^2)/numel(outwf)); % For debuging.
+    drawnow
+end
+
+
+function cost = cost_function2(pos,wf,mirror,FF)
+% Returns FF Quality metric from solver position
+    v = pos*100; % Solver coord space -> voltage space
+    mirror.set_channels(v);
+    outwf = wf + mirror.shape;
+    ff = FF.generate_farfield(1,outwf)*1e-6;
+    ff = medfilt2(ff,[3 3]); % Filter to reduce noise
     
-    % Converges on the value of cost that you want.
-     [ooesPercent,ooesCircDiam]= oneOverESquared(ff);
-    cost = ooesCircDiam
-    want = 11;
-    Test = round(cost,1);
-    
-    if Test == want
-        return
-    elseif Test > want
-        cost = cost;
-    elseif Test < want
-        cost = - cost;
-        if cost < -want
-            cost = -cost;
-        end
-    end
-    
+    cost = Errorfunct(ff,60); %Set what you are aiming for here
+    Test = round(cost,0)
+%     if Test == 0
+%         return
+%     end
     % cost = sqrt(sum(outwf(:).^2)/numel(outwf)); % For debuging.
     drawnow
 end
@@ -153,3 +227,4 @@ function plotFF(ff)
     colormap jet
     colorbar
 end
+
