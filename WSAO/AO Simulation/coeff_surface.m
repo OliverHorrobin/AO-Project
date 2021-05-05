c7676e36f5707fcca2d1e1b0fe523c8ac176bfa9
OliverHorrobin <74053426+OliverHorrobin@users.noreply.github.com>
2021-05-05 16:58:23 +0100

    Delete WSAO directory

--- WSAO/AO Simulation/coeff_surface.m
+++
@@ -1,8 +0,0 @@
-function surface = coeff_surface(coeffs,polys)
-    % Generates surface given the coefficients vector and polynomials
-    if isrow(coeffs)
-        coeffs = coeffs';
-    end
-    coeffs = permute(coeffs,[3,2,1]);
-    surface = sum(coeffs.*polys,3);
-end
